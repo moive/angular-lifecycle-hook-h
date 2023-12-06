7428ae01468fa7804d1359825ebca4a1501db313
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-05 21:49:48 -0500

    implement lifecycle hooks

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.sass'],
 })
-export class ProductPageComponent {}
+export class ProductPageComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  public isProductVisible: boolean = false;
+
+  constructor() {
+    console.log('constructor...');
+  }
+
+  ngOnInit(): void {
+    console.log('gOnInit 😀');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('gOnChanges 😀');
+  }
+  ngDoCheck(): void {
+    console.log('gDoCheck 😀');
+  }
+  ngAfterContentInit(): void {
+    console.log('gAfterContentInit 😀');
+  }
+  ngAfterContentChecked(): void {
+    console.log('gAfterContentChecked 😀');
+  }
+  ngAfterViewInit(): void {
+    console.log('gAfterViewInit 😀');
+  }
+  ngAfterViewChecked(): void {
+    console.log('gAfterViewChecked 😀');
+  }
+  ngOnDestroy(): void {
+    console.log('gOnDestroy 😀');
+  }
+}
